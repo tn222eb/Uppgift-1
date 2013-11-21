32a3dd2f810a809e4d7b7df3861dadce2b50c2d1
tn222eb <tn222eb@student.lnu.se>
2013-11-21 23:24:51 +0100

    Ändrat i whitebox testning

--- Triangel/kod/c#/Tri/UnitTestProject/UnitTest1.cs
+++ Triangel/kod/c#/Tri/UnitTestProject/UnitTest1.cs
@@ -8,12 +8,42 @@ namespace UnitTestTri
     {
         [TestMethod]
 
-           public void isIsosceles()
+           public void isIsoscelesthreeDoubles()
         {
             // Förväntar mig att det ska bli true eftersom gett rätt värde till en likbent triangel
-            Triangle triangleTest3 = new Triangle(1.0, 0.0, 1.0);
+            Triangle triangleTest3 = new Triangle(1.0, 2.0, 1.0);
             Assert.IsTrue(triangleTest3.isIsosceles());
 
+        }
+        [TestMethod]
+
+        public void isIsoscelesDoubleArray()
+        {
+            // Förväntar mig att det ska bli true eftersom gett rätt värde till en likbent triangel
+            Triangle triangleTest3 = new Triangle(new double []{ 1.0, 2.0, 1.0});
+            Assert.IsTrue(triangleTest3.isIsosceles());
+
+        }
+
+        [TestMethod]
+        public void isScalenePoint()
+        { 
+            Point a = new Point(12, 15);
+            Point b = new Point(13, 17);
+            Point c = new Point(10, 12);
+            Triangle Triangletestnew = new Triangle(a, b, c);
+            Assert.IsTrue(Triangletestnew.isScalene());
+        }
+
+        [TestMethod]
+
+        public void isScalenePointArray()
+        { 
+            Point a = new Point(12, 15);
+            Point b = new Point(13, 17);
+            Point c = new Point(10, 12);
+            Triangle triangleTestArray = new Triangle(new Point[] { a, b, c });
+            Assert.IsTrue(triangleTestArray.isScalene());
         }
 
         [TestMethod]
@@ -30,11 +60,11 @@ namespace UnitTestTri
 
         [TestMethod]
 
-        public void isScalene()
+        public void isEquilateral()
         {
             // Förväntar mig att det ska bli false eftersom gett fel värde till en triangel med olika sidor
             Triangle triangleTest2 = new Triangle(1.0, 1.0, 1.0);
-            Assert.IsTrue(triangleTest2.isScalene());
+            Assert.IsTrue(triangleTest2.isEquilateral());
         }
 
         [TestMethod]
@@ -48,11 +78,11 @@ namespace UnitTestTri
 
         [TestMethod]
 
-        public void isEquilateral()
+        public void isScalene()
         {
             // Förväntar mig att det ska bli false eftersom gett fel värde till en liksidig triangel
             Triangle triangleTest6 = new Triangle(2.0, 0.6, 0.4);
-            Assert.IsTrue(triangleTest6.isEquilateral());
+            Assert.IsTrue(triangleTest6.isScalene());
         }
             
     }
